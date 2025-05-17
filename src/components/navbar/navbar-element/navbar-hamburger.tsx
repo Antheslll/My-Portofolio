d09731a1dd7d0d394e8d53804d4923ffd6d8fd42
Antheslll <liemanthony27@gmail.com>
2025-05-17 07:09:55 +0700

    merubah navbar hamburger menu

--- src/components/navbar/navbar-element/navbar-hamburger.tsx
+++ src/components/navbar/navbar-element/navbar-hamburger.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 
 interface NavbarHamburgerProps {
@@ -12,14 +13,12 @@ const NavbarHamburger = ({
   handleCloseOverlay,
 }: NavbarHamburgerProps) => {
   return (
-    <Link href={link} onClick={handleCloseOverlay}>
-      <li
-        onClick={handleCloseOverlay}
-        className="poppins-font font-semibold text-[6vw] list-none text-black hover:text-[#3B82F6] hover:font-bold hover:text-[8vw] transition-all duration-200 ease-in-out"
-      >
-        {nav}
-      </li>
-    </Link>
+    <li
+      onClick={() => handleCloseOverlay()}
+      className="poppins-font font-semibold text-[6vw] list-none text-black hover:text-[#3B82F6] hover:font-bold hover:text-[8vw] transition-all duration-200 ease-in-out"
+    >
+      <Link href={link}>{nav}</Link>
+    </li>
   );
 };
 
